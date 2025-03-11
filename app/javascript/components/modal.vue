9bc3740d6e27133c19853bd719e0f3dd5343ee1b
R-Tok <ddrer.toqn@gmail.com>
2025-03-11 21:21:45 +0900

    市町村マップを再度leafletに変更

--- app/javascript/components/modal.vue
+++ app/javascript/components/modal.vue
@@ -1,35 +1,23 @@
 <template>
   <div>
     <button @click="openModal" class="btn"  :class="buttonClass">
-      <template v-if="buttonText === 'aaa'">
-        <i class="bi bi-bookmark-heart" style="font-size: 18px;"></i> <!-- buttonTextが'aaa'の場合はアイコンを表示 -->
-      </template>
-      <template v-else>
-        {{ buttonText }}
-      </template>
+
+      {{ buttonText }}
       
     </button>
 
     <ModalDescription v-if="type === 'description'" :show="showModal" @close="showModal = false" />
-    <ModalList
-      v-if="type === 'list'"
-      :show="showModal"
-      :post="post"
-      :loggedIn="loggedIn"
-      :userLists="userLists"
-      :userListsPath="userListsPath"
-      @close="closeModal"
-    />
+
   </div>
 </template>
 
 <script>
 import ModalDescription from './ModalDescription.vue'
-import ModalList from './ModalList.vue'
+
 
 export default {
   components: {
-    ModalDescription, ModalList
+    ModalDescription
   },
   props: {
     type: String,  // 'description' または 'list'が入る
@@ -45,10 +33,10 @@ export default {
   },
   computed: {
     buttonText() {
-      return this.type === 'description' ? 'このアプリについて' : 'aaa'
+      return 'このアプリについて'
     },
     buttonClass() {
-      return this.type === 'description' ? 'btn-success' : 'btn-sm rounded-pill btn-light'
+      return 'btn-success'
     }
   },
   methods: {

9097346dbde94c04c4e4aaa9499f785a8c0f9b38
R-Tok <ddrer.toqn@gmail.com>
2025-03-14 19:20:39 +0900

    メールアドレス変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,6 @@ class UserMailer < ApplicationMailer
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
 
-    mail(to: user.email, subject: "パスワードリセット")
+    mail(to: user.email, subject: "【LocalTripHub】パスワード再設定のお知らせ")
   end
 end
